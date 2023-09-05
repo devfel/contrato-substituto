2e5411671d09368658d8337bffc3b1010de1950a
devfel <devfelizardo@gmail.com>
2023-09-04 23:23:41 -0300

    Cleaning script that had unecessary code

--- script.js
+++ script.js
@@ -11,19 +11,5 @@ document.getElementById("textField").addEventListener("input", function () {
 });
 
 document.getElementById("generatePDF").addEventListener("click", function () {
-  let elementsToHide = document.querySelectorAll(".hide-for-pdf");
-  elementsToHide.forEach((el) => {
-    el.style.display = "none";
-  });
-
-  document.getElementById("textFieldValue").innerText =
-    document.getElementById("textField").value;
-
   window.print();
-
-  setTimeout(function () {
-    elementsToHide.forEach((el) => {
-      el.style.display = "";
-    });
-  }, 500);
 });
